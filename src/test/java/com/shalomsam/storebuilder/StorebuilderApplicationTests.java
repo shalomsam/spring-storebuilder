6242918abe3ce7c9cc1f88f8724d5804eff76d06
Shalom Sam <juz.cool1@gmail.com>
2023-11-01 01:24:03 -0700

    Initial commit

---
+++ src/test/java/com/shalomsam/storebuilder/StorebuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalomsam.storebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorebuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
